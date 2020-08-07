8af303bc6e6d573354e96c8ce831fbad22946d11
Jordan Duran <jordanduran@static-192-165-1-12.cust.crystone.se>
2020-08-06 22:40:23 -0400

    handleParamChange function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 import './App.css';
 
 function App() {
@@ -10,11 +11,25 @@ function App() {
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
   return (
     <Container className='my-4'>
       <h1 className='mb-4'>GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-      {loading && <h1>Loading...</h1>}
+      {loading && (
+        <Spinner animation='border' role='status'>
+          <span className='sr-only'>Loading...</span>
+        </Spinner>
+      )}
       {error && <h1>Error, try refreshing!</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
